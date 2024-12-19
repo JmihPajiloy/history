481ff154c2445725ed1447971ad29595434500e5
jmih_pajiloy <svanyokshib@gmail.com>
2024-12-19 22:46:17 +0300

    add: markdown support

--- app/articles/layout.tsx
+++ app/articles/layout.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <main className="py-4 px-48">
+    <main className="py-4 px-48 grow">
       {children}
     </main>
   );
