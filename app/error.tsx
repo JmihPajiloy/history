dd04c2015ca71be4d8c5f9062df3c990636b90c0
jmih_pajiloy <svanyokshib@gmail.com>
2025-03-28 01:30:13 +0300

    add: global error handling

---
+++ app/error.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { CircleAlert, RotateCcw, Undo2 } from "lucide-react";
+import Link from "next/link";
+import { useEffect } from "react";
+
+export default function Error({
+                                error,
+                                reset
+                              }: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <main className="flex grow mx-auto flex-col items-center justify-center gap-2 w-56">
+      <CircleAlert className="size-20 mb-4" />
+      <h2 className="text-center font-semibold text-xl">Что-то пошло не так!</h2>
+      <Button variant="outline" className="w-full" onClick={() => reset()}>
+        Попробовать еще раз <RotateCcw className="ml-2 size-4 "/>
+      </Button>
+      <Button className="w-full" asChild>
+        <Link href="/">
+          Вернуться на главную <Undo2 className="ml-2 size-4 -translate-x-2"/>
+        </Link>
+      </Button>
+    </main>
+  );
+}
