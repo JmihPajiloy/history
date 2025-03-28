7e154b78fffe10e80eea5c0ddaede04ab83c76d8
jmih_pajiloy <svanyokshib@gmail.com>
2025-03-28 03:54:17 +0300

    add: button animation

--- components/back-to-main.tsx
+++ components/back-to-main.tsx
@@ -10,19 +10,17 @@ import { animated, useSpring, config } from "@react-spring/web";
 export const BackToMain = ({ className }: { className?: string }) => {
   const [styles, api] = useSpring(() => ({
     from: { x: 0 },
-    to: {x: 1},
-    config: config.default,
-    delay: 0
+    to: async next => next({ x: 5 }),
+    config: config.wobbly, reverse: true
   }));
 
   return (
     <div className={cn("flex py-4", className)}>
-      <Button variant="default" asChild onMouseEnter={() => api.start()}>
+      <Button variant="default" asChild onMouseEnter={() => api.start({
+        from: { x: 0 }
+      })}>
         <Link href="/">
-          <animated.div style={{x: styles.x.to(
-            [0, 0.5, 1],
-            [0, 5, 0]
-          )}}>
+          <animated.div style={styles}>
             <ArrowLeft className="mr-1 size-4" />
           </animated.div>
           На главную
